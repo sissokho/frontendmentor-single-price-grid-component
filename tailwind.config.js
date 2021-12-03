f5a7b23509b7b67003efabf9aa8f6ee48c92b230
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2021-12-03 18:25:44 +0000

    Upate style guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,8 @@ module.exports = {
             cyan: 'hsl(179, 62%, 43%)',
             yellow: 'hsl(71, 73%, 54%)',
             gray: 'hsl(204, 43%, 93%)',
-            blue: 'hsl(218, 22%, 67%)'
+            blue: 'hsl(218, 22%, 67%)',
+            white: 'hsl(0, 0%, 100%)'
         },
         fontFamily: {
             karla: ['Karla', 'sans-serif']
@@ -19,7 +20,11 @@ module.exports = {
             regular: 400,
             bold: 700
         },
-        extend: {},
+        extend: {
+            spacing: {
+                '88': '22rem'
+            }
+        },
     },
     variants: {
         extend: {},
