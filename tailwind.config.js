aeca7f9d213943a185355a7bbb22ac9f8d779fb1
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2023-07-13 17:46:11 +0000

    Update tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,28 @@
 module.exports = {
-    purge: ['index.html'],
-    darkMode: false, // or 'media' or 'class'
+    content: ["index.html"],
     theme: {
         screens: {
-            tablet: '768px'
+            tablet: "768px",
         },
         colors: {
-            cyan: 'hsl(179, 62%, 43%)',
-            yellow: 'hsl(71, 73%, 54%)',
-            gray: 'hsl(204, 43%, 93%)',
-            blue: 'hsl(218, 22%, 67%)',
-            white: 'hsl(0, 0%, 100%)'
+            cyan: "hsl(179, 62%, 43%)",
+            yellow: "hsl(71, 73%, 54%)",
+            gray: "hsl(204, 43%, 93%)",
+            blue: "hsl(218, 22%, 67%)",
+            white: "hsl(0, 0%, 100%)",
         },
         fontFamily: {
-            karla: ['Karla', 'sans-serif']
+            karla: ["Karla", "sans-serif"],
         },
         fontWeight: {
             regular: 400,
-            bold: 700
+            bold: 700,
         },
         extend: {
             spacing: {
-                '88': '22rem'
-            }
+                88: "22rem",
+            },
         },
     },
-    variants: {
-        extend: {},
-    },
     plugins: [],
-}
+};
